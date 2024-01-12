1cb3f86aa007f825cbfb7d29c6083ba045e81eff
Harshit Jain <harshitostwal1234@gmail.com>
2024-01-13 02:07:24 +0530

    Updated title in metadata

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Upscaler Ai - Image Enchancer',
+  title: 'Upscaler Ai - Image Enchancer | Harshit Ostwal',
   description: 'Experience stunning visual enhancements with our Upscaler AI – the ultimate image enhancer. Boost image resolution, reduce pixelation, and unveil hidden details in your photos. Transform ordinary visuals into extraordinary masterpieces effortlessly.',
 }
 
