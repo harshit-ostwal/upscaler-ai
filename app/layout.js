92f1a82cb18b469aa4724a799c6b02c9209cfe1d
Harshit Jain <harshitostwal1234@gmail.com>
2024-01-13 02:03:35 +0530

    Upscaler Ai - Image Enchancer | Harshit Ostwal

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Upscaler Ai - Image Enchancer',
+  description: 'Experience stunning visual enhancements with our Upscaler AI – the ultimate image enhancer. Boost image resolution, reduce pixelation, and unveil hidden details in your photos. Transform ordinary visuals into extraordinary masterpieces effortlessly.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
